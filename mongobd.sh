f318c48f109fd82f2c8e7e99e344c0401c1cf8e4
vinay <vinaythativati@gmail.com>
2026-05-30 16:42:57 +0530

    mongo

---
+++ mongobd.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+LOGS="/var/log/roboshop"
+sudo mkdir -p $LOGS
+sudo chown -R vinay:vinay $LOGS
+sudo chmod -R 755 $LOGS
+file="$LOGS/$0.log".
+
+
+time=$(date '+%Y-%m-%d %H:%M:%S')
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color (Reset)
+
+
+user=$(id -u)
+
+if [ $user -ne 0 ]; then
+        echo "$time [ERROR] $R run with super user $N" | tee -a $file
+        exit 1
+fi
+validate() {
+
+        if [ $1 -ne 0 ]; then
+                echo "$time [ERR] $2 .. $RED failure" | tee -a $file
+
+        else
+                echo "$time  [INFO] $2 .. $GREEN success" | tee -a $file
+        fi
+
+}
+
+cp mongo.repo /etc/apt/sources.list.d/
+validate $? "Adding repo"
+
