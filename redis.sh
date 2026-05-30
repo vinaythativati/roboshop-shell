41e13c8831d1ad284eeacfba2d00337045f1e22b
vinay <vinaythativati@gmail.com>
2026-05-31 00:05:07 +0530

    push

---
+++ redis.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+LOGS="/var/log/roboshop"
+sudo mkdir -p $LOGS
+sudo chown -R ec2-user:ec2-user $LOGS
+sudo chmod -R 755 $LOGS
+file="$LOGS/$0.log".
+
+
+time=$(date '+%Y-%m-%d %H:%M:%S')
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color (Reset)
+
+
+user=$(id -u)
+
+if [ $user -ne 0 ]; then
+        echo -e "$time [ERROR] $R run with super user $NC" | tee -a $file
+        exit 1
+fi
+validate() {
+
+        if [ $1 -ne 0 ]; then
+                echo -e "$time [ERR] $2 .. $RED Failure $NC" | tee -a $file
+
+        else
+                echo -e "$time  [INFO] $2 .. $GREEN success $NC" | tee -a $file
+        fi
+
+}
+
+dnf module disable redis -y
+dnf module enable redis:7 -y
+dnf install redis -y &>> $file
+validate $? "Installing Redis"
+
+
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no'   /etc/redis/redis.conf
+validate $? "Updating Redis config"
+
+systemctl enable --now redis 
+validate $? "Restarting Redis"
+
